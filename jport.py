eb819d63d2dc928b155fc84f5d1b3086f35d308d
Joseph B <96390676+webn3ewbie@users.noreply.github.com>
2022-12-17 12:06:59 -0500

    Update jport.py

--- jport.py
+++ jport.py
@@ -106,7 +106,7 @@ if selected == "Blog":
             font-family: 'MERRIWEATHER';
             }
             </style> """)
-        components.iframe("https://mirror.xyz/0x05721B0aD76eC33DefB59Abd613E30FbF4b4127C/tnyzAnLPyOp7f1Jqt01MqOZ7MlmmEswyHC7iz0S3mFA/?embedded=true",height =800)
+        components.iframe("https://mirror.xyz/0x05721B0aD76eC33DefB59Abd613E30FbF4b4127C",height =800)
         
 # Contact
 ######################    
