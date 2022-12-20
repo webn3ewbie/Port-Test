95707c47d865c9acc5a484b887c0245410c6eb69
Joseph B <96390676+webn3ewbie@users.noreply.github.com>
2022-12-20 14:04:41 -0500

    Update jport.py

--- jport.py
+++ jport.py
@@ -99,10 +99,8 @@ if selected == "Blog":
     with st.container():
         st.header("[Blog Hosted On Blockchain](https://mirror.xyz/0x05721B0aD76eC33DefB59Abd613E30FbF4b4127C)")
         url = "https://mirror.xyz/0x05721B0aD76eC33DefB59Abd613E30FbF4b4127C"
-
-        st.header("Website Embedded in an iframe")
-
-        components.iframe(url, height=500)
+        html = f'<iframe src="{url}" width="800" height="600"></iframe>'
+        st.markdown(html, unsafe_allow_html=True)
     
         
 # Contact   
